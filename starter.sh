a04dc40fc350b9a0d50809ebe7c4fa3dd08d9a43
Piotr Rawicki <piotrekraw@o2.pl>
2023-01-20 17:01:13 +0100

    node version corrected

--- starter.sh
+++ starter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-PATH=/root/.nvm/versions/node/v16.18.0/bin:/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+PATH=/root/.nvm/versions/node/v16.18.1/bin:/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 mkdir /root/.bittensor/wallets && cp -r /workspace/wallets_backup/* /root/.bittensor/wallets && cp /workspace/.bashrc  /root/.bashrc  && cp /workspace/.bash_history  /root/.bash_history
 pm2 resurrect
 sleep infinity
