2be1bb3e89e2ff09d87c328cc9464ef8ba3d0232
Piotr Rawicki <piotrekraw@o2.pl>
2023-01-20 15:54:57 +0100

    added backup for pm2 and resurect

--- backup_script.sh
+++ backup_script.sh
@@ -1,1 +1,7 @@
 mkdir /workspace/wallets_backup && cp -r /root/.bittensor/wallets/* /workspace/wallets_backup && cp /root/.bashrc /workspace/ && cp /root/.bash_history /workspace/
+pm2 save
+mkdir /workspace/pm2 && cp -r /root/.pm2/* /workspace/pm2
+pm2 stop all
+pm2 delete all
+export PM2_HOME=/workspace/pm2
+pm2 resurrect
