aa3ea6af0ca7356a90877a7b9a40ca8e725a7445
Piotr Rawicki <piotrekraw@o2.pl>
2023-01-20 13:01:37 +0100

    v2

--- starter.sh
+++ starter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 PATH=/root/.nvm/versions/node/v16.18.0/bin:/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-cp -r /workspace/wallets_backup/* /root/.bittensor/wallets
-PM2_HOME=/workspace
+cp -r /workspace/wallets_backup/* /root/.bittensor/wallets && cp /workspace/.bashrc  /root/.bashrc  && cp /workspace/.bash_history  /root/.bashrc
+export PM2_HOME=/workspace/pm2
 pm2 resurrect 
 sleep infinity
